780675aa21d7749adeab68259a4281a9438be96e
Nicholas Ao <aominhlong@yahoo.com>
2022-03-27 16:01:05 -0600

    Add get recipe by tag method

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,8 +1,25 @@
 class RecipeRepository {
-  constructor() {
-
-    // One class to get you started!
+  constructor(data) {
+    this.data = data;
+    this.recipes = [];
+    console.log('here', this.data[0].tags)
   }
+  getRecipeByTag(tag) {
+    const filteredRecipe = this.data.filter((recipe) => {
+      if (recipe.tags.includes(tag)) {
+        this.recipes.push(recipe)
+      }
+    })
+  }
+
+  // goal: an array that is based on the tag
+  // input: recipe data array
+  // output: an array that is based on the tag
+  // Steps: 
+  // push recipes if tag matches into the recipe tag
 }
+
+
+
 
 export default RecipeRepository;
--- test/RecipeRepository-test.js
+++ test/RecipeRepository-test.js
@@ -1,8 +1,25 @@
 import { expect } from 'chai';
 import RecipeRepository from '../src/classes/RecipeRepository';
+import recipeData from '../src/data/recipes.js';
 
 describe('Recipe', () => {
-  it('Should be a function', () => {
+
+  let recipeRepository;
+
+  beforeEach(() => {
+    recipeRepository = new RecipeRepository(recipeData)
+  })
+
+  it.skip('Should be a function', () => {
     expect(RecipeRepository).to.be.a('function');
   });
+  it.skip('should have the recipes', () => {
+    expect(recipeRepository.data).to.equal(recipeData)
+  })
+  it('should filter recipe by tag', () => {
+
+    recipeRepository.getRecipeByTag(`hor d'oeuvre`)
+    console.log(recipeRepository.recipes)
+    expect(recipeRepository.recipes.length).to.equal(9)
+  })
 })
