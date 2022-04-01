b0fc38c8d0db659536c21d40f64a360f1ea9b473
Nicholas Ao <aominhlong@yahoo.com>
2022-03-31 20:27:53 -0600

    Add user methods

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,13 +1,48 @@
 class User {
     constructor(userData) {
         this.favoriteRecipes = []
-        this.name = userData.name //
+        this.filteredFavoriteRecipes = []
+        this.name = userData.name 
         this.id = userData.id
         this.pantry = userData.pantry
         this.recipesToCook = []
     }
 
+    favoriteRecipe(recipe) {
+        if(!this.favoriteRecipes.includes(recipe)){
+            this.favoriteRecipes.push(recipe)
+        }
+    }
 
+    unfavoriteRecipe(recipe) {
+        this.favoriteRecipes.forEach((favoriteRecipe) => {
+            if (favoriteRecipe.id === recipe.id) {
+                this.favoriteRecipes.splice(favoriteRecipe)
+            }
+        })
+    }
+
+    decideToCook(recipe) {
+        if(!this.recipesToCook.includes(recipe)){
+            this.recipesToCook.push(recipe)
+        }
+    }
+
+    filterFavoriteRecipeByTag(tag) {
+        this.favoriteRecipes.filter((recipe) => {
+            if (recipe.tags.includes(tag)) {
+                this.filteredFavoriteRecipes.push(recipe)
+            }
+        })
+    }
+
+    filterFavoriteRecipeByName(name) {
+        this.favoriteRecipes.filter((recipe) => {
+            if (recipe.name.includes(name)) {
+                this.filteredFavoriteRecipes.push(recipe)
+            }
+        })
+    }
 
 }
 
